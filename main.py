c49438213aea9c71934b9289fad6ef440c91dceb
Abdul-Rahman Ibrahim <abdulrahmanibrahim.ish@gmail.com>
2025-07-25 17:08:14 +0300

    added king class

--- main.py
+++ main.py
@@ -359,9 +359,70 @@ class Pawn(Board):
         return scopes #empty = promoting
 
 
-class King(Queen):
+class King(Board):
+    def get_right_scope(self, file: str, rank: int):
+        if self.is_on_right_end(file, rank):
+            return []
+        next_file = self.files[self.files.index(file) + 1]
+        return [f'{next_file}{rank}']
+    
+    def get_top_scope(self, file: str, rank: int):
+        if self.is_on_top_end(file, rank):
+            return []
+        next_rank = self.ranks[self.ranks.index(rank) + 1]
+        return [f'{file}{next_rank}']
+    
+    def get_left_scope(self, file: str, rank: int):
+        if self.is_on_left_end(file, rank):
+            return []
+        next_file = self.files[self.files.index(file) - 1]
+        return [f'{next_file}{rank}']
+    
+    def get_bottom_scope(self, file: str, rank: int):
+        if self.is_on_bottom_end(file, rank):
+            return []
+        next_rank = self.ranks[self.ranks.index(rank) - 1]
+        return [f'{file}{next_rank}']
+    
+    def get_top_right_scope(self, file: str, rank: int):
+        if self.is_on_right_end(file, rank) or self.is_on_top_end(file, rank):
+            return []
+        next_file = self.files[self.files.index(file) + 1]
+        next_rank = self.ranks[self.ranks.index(rank) + 1]
+        return [f'{next_file}{next_rank}']
+    
+    def get_top_left_scope(self, file: str, rank: int):
+        if self.is_on_left_end(file, rank) or self.is_on_top_end(file, rank):
+            return []
+        next_file = self.files[self.files.index(file) - 1]
+        next_rank = self.ranks[self.ranks.index(rank) + 1]
+        return [f'{next_file}{next_rank}']
+
+    def get_bottom_right_scope(self, file: str, rank: int):
+        if self.is_on_right_end(file, rank) or self.is_on_bottom_end(file, rank):
+            return []
+        next_file = self.files[self.files.index(file) + 1]
+        next_rank = self.ranks[self.ranks.index(rank) - 1]
+        return [f'{next_file}{next_rank}']
+
+    def get_bottom_left_scope(self, file: str, rank: int):
+        if self.is_on_left_end(file, rank) or self.is_on_bottom_end(file, rank):
+            return []
+        next_file = self.files[self.files.index(file) - 1]
+        next_rank = self.ranks[self.ranks.index(rank) - 1]
+        return [f'{next_file}{next_rank}']
+    
     def get_scopes(self, file: str, rank: int):
-        pass
+        scopes = []
+        scopes = scopes + self.get_top_scope(file, rank)
+        scopes = scopes + self.get_right_scope(file, rank)
+        scopes = scopes + self.get_left_scope(file, rank)
+        scopes = scopes + self.get_bottom_scope(file, rank)
+        scopes = scopes + self.get_top_right_scope(file, rank)
+        scopes = scopes + self.get_top_left_scope(file, rank)
+        scopes = scopes + self.get_bottom_right_scope(file, rank)
+        scopes = scopes + self.get_bottom_left_scope(file, rank)
+        return scopes
 
 
 
@@ -370,20 +431,20 @@ class King(Queen):
 files = 'abcdefgh'
 ranks = '12345678'
 
-piece = Pawn()
+piece = King()
 for f in files:
     for r in ranks:
         print(f'{f}{r}-->{piece.get_scopes(f, int(r))}')
 
-# f = 'h'
-# r = 8 
-# # board = Board()
-# # print(board.is_on_top_end('e', 8))
-# # queen = Queen()
-# # print(queen.get_scopes('b', 7))
-# # bishop = Bishop()
-# # print(bishop.get_scopes('g', 5))
-# knight = Knight()
-# print(f'{f}{r} --> {knight.get_scopes(f, r)}')
+# f = 'a'
+# r = 8
+# board = Board()
+# print(board.is_on_top_end('e', 8))
+# queen = Queen()
+# print(queen.get_scopes('b', 7))
+# bishop = Bishop()
+# print(bishop.get_scopes('g', 5))
+# piece = King()
+# print(f'{f}{r} --> {piece.get_scopes(f, r)}')
 
         
